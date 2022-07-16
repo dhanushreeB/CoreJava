817fde202ee69635ed5cd002bec3142234785be9
Dhanushree B <86241933+dhanushreeB@users.noreply.github.com>
2022-07-16 08:07:43 +0530

    Add files via upload

---
+++ Collections1.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class Collections1 {
+    public static void main(String[] args) {
+        LinkedList<String> ll = new LinkedList<>();
+        ll.add("dhanu");
+        ll.add("shree");
+        System.out.println(ll);
+        System.out.println(ll.size());
+
+    }
+}
